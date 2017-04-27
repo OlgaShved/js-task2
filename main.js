c9d02328f104d0b006ed796d83fa4a3da7d4a573
OlgaShved <voiceofmind.cn@gmail.com>
2017-04-27 03:29:06 +0300

    task_2_and_3

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-function FindPrimeNumbers(start, end){
-    if(start > end){
+
+/* Исправленное задание №2 */
+
+function FindPrimeNumbers(start, end) {
+
+    if(start > end) {
         alert("Начальное значение должно быть меньше или равно конечному значению");
         return;
     }
 
-    if(start < 2){
+    if(start < 2) {
         start = 2;
     }
 
@@ -20,11 +24,30 @@ function FindPrimeNumbers(start, end){
         }
 
         //если число ни на что не делится, то оно простое
-        if(PrimeNumber){
-            document.write(i);
+        if(PrimeNumber) {
+            document.write(i + " ");
         }
     }
 }
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+var firstNumber = prompt("Введите начальное значение");
+if (isNumeric(firstNumber)) {
+    var lastNumber = prompt("Введите конечное значение");
+    if (isNumeric(lastNumber)) {
+        FindPrimeNumbers(firstNumber, lastNumber);
+    } else {
+        alert('Некорректое значение');
+    }
+
+} else {
+    alert('Некорректое значение');
+}
+
+
 
 
 /* на уроке можно было не определять пустоту, а просто указать maxNamе? */
@@ -47,3 +70,22 @@ var maxName = "нет сотрудников";
         }
     }
 document.write(maxName);
+
+
+/* ДЗ №3 */
+function square(x) { return x * x; } // возведение в квадрат
+
+function map(fn, array) {
+    var resultArray = [];
+    for(var i=0; i<array.length; i++) {
+        resultArray[i] = fn(array[i]);
+    }
+    return resultArray;
+}
+
+console.log(map(square, [1, 2, 3, 4])); // [1, 4, 9, 16]
+console.log(map(square, [])); // []
+
+var arr = [1, 2, 3];
+console.log(map(square, arr)); // [1, 4, 9]
+console.log(arr); // [1, 2, 3]
